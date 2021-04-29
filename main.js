f829e68dbeebc2f86fec5bf7d5e8566bc3515d73
r-tobias <rebeccatobias123@gmail.com>
2021-04-29 18:24:29 -0400

    added for loop and some documentation

--- main.js
+++ main.js
@@ -13,6 +13,13 @@ function convertNumToPhonetic(arr) {
     "Nine",
   ];
   let characters = arr.toString();
+  for (let i = 0; i < characters.length; i++) {
+    if (words[parseInt(characters[i])] != undefined) {
+      // check if it's not a comma
+      answer += words[parseInt(characters[i])];
+      // the value of each element in the words array corresponds to its index number
+    } else answer += ",";
+  }
 
   return answer;
 }

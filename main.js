acabadb0bb589019d1edf248e45a05f515b9a8bb
r-tobias <rebeccatobias123@gmail.com>
2021-04-29 18:20:41 -0400

    initialized variables

---
+++ main.js
@@ -0,0 +1,18 @@
+function convertNumToPhonetic(arr) {
+  let answer = "";
+  let words = [
+    "Zero",
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+  ];
+  let characters = arr.toString();
+
+  return answer;
+}
